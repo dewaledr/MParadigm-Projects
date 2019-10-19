26c0691a6d75667bdc9671c54a28a26368497c14
Francis Adepoju <francis.adepoju@hotmail.com>
2019-10-19 21:27:39 +0800

    Added Java Folder

---
+++ JavaProgs/Person.java
@@ -0,0 +1,53 @@
+// public class Person{
+// 	private String name;
+// 	private int age;
+//
+// 	public Person(String n, int a){
+// 		this.name = n;
+// 		this.age = a;
+// 	}
+//
+// 	public String toString(){
+// 	return ("Name: " + this.name + "\tAge: " + this.age);
+// 	}
+//
+// 	public static void main(String[] args){
+// 		Person p1 = new Person("Francis", 21);
+// 		Person p2 = new Person("Catherine", 23);
+//
+// 		System.out.println(p1);
+// 		System.out.println(p2);
+// 	}
+// }
+
+public class Person{
+	private String name;
+	private int age;
+	
+	public Person(String n, int a){
+		this.name = n;
+		this.age = a;
+	}
+	
+	public void setAge(int g){
+		if (g < 0 || g > 120){	//Reasonable age range for humans
+			System.out.println("Age still remain: " + this.age);
+			//Do not allow negative or stupid age changes
+			return;
+		}
+		this.age = g;
+	}
+	
+	public String toString(){
+	return ("Name: " + this.name + "\tAge: " + this.age);
+	}
+	
+//	public static void main(String[] args){
+//		Person p1 = new Person("Francis", 21);
+//		Person p2 = new Person("Catherine", 23);
+//		//p1.age = -1;
+//		p1.setAge(-1);
+//		System.out.println(p1);
+//		System.out.println(p2);
+//	}
+}
---
+++ JavaProgs/Student.java
@@ -0,0 +1,25 @@
+
+public class Student extends Person{
+	private String[] classes;
+	
+	public Student(String n, int a, String[] c){
+		super(n,a);
+		this.classes = c;
+	}
+	
+	public String toString(){
+		//Overwite toString from parent and add new detail to it.
+		String repr = super.toString() + "\nClasses: \n";
+		for (int i = 0; i < classes.length; i++){
+			repr += classes[i] + "\n";
+		}
+		return repr;
+	}
+	
+	public static void main(String[] args){
+		String[] classes = new String[]{"Python programming", "Programming Design","IT Project Management"};
+		Student s = new Student("Wang MangMang",34,classes);
+		s.setAge(100);
+		System.out.println(s);
+	}
+}
