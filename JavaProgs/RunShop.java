be1cc50b49b19d52f6d5db95b72fcda80c5866a7
Francis Adepoju <francis.adepoju@hotmail.com>
2019-11-02 16:59:17 +0800

    Cracked Option Selection

--- JavaProgs/RunShop.java
+++ JavaProgs/RunShop.java
@@ -7,61 +7,71 @@ public class RunShop {
 	public static void main(String[] args) {
 		{
 			
-			var keyboard = new Scanner(System.in);
+			//int ans;
+			String str;
+			
+			Scanner sin = new Scanner(System.in);
+//			System.out.println("Enter your Choice: ");
+//			str = sin.nextLine();
+//			System.out.println("You entered string " + str);
+//			sin.close();
 
 			
 			
 			//char resp = '0';
 			System.out.println("\n");
 			System.out.println("\n");
-			System.out.println("***************************************************************************************\n");
-			System.out.println("***        GMIT JAVA Shop, Inc. Shop Manager on Duty today is: Francis Adepoju       ***\n");
-			System.out.println("***        *******************************************************************      ***\n");
-			System.out.println("***                       PLEASE ENTER YOUR DESIRED OPTION                          ***\n");	
-			System.out.println("***                       Enter  0 ==> End the Program,                             ***\n");
-			System.out.println("***                       Enter  1 ==> Single Item Purchase                         ***\n");            
-			System.out.println("***                       Enter  2 ==> Multiple Purchase from .CSV file             ***\n"); 
-			System.out.println("***                                                                                 ***\n");
-			System.out.println("***************************************************************************************\n");
-			System.out.println("***************************************************************************************\n");
-			System.out.println("Choice: ");
-			//scanf("%s", &resp);
-			//System.out.println("enter an integer");
-			//String myInput = keyboard.nextLine();
+			System.out.println("***************************************************************************************");
+			System.out.println("***        GMIT JAVA Shop, Inc. Shop Manager on Duty today is: Francis Adepoju      ***");
+			System.out.println("***        *******************************************************************      ***");
+			System.out.println("***                       PLEASE ENTER YOUR DESIRED OPTION                          ***");	
+			System.out.println("***                       Enter  0 ==> End the Program,                             ***");
+			System.out.println("***                       Enter  1 ==> Single Item Purchase                         ***");            
+			System.out.println("***                       Enter  2 ==> Multiple Purchase from .CSV file             ***"); 
+			System.out.println("***                                                                                 ***");
+			System.out.println("***************************************************************************************");
+			System.out.println("***************************************************************************************");
+			//System.out.println("Choice: ");
 			
-			String myInput = keyboard.next();
-			while (!myInput.isEmpty() || !myInput.isBlank() ){
-				if (myInput == "1" ){
+			System.out.println("Enter your Choice: ");
+			str = sin.nextLine();
+			//System.out.println("You entered string " + str);
+			//sin.close();
+			
+
+			// While str does not match regex zero, the while loop is active unless str is also not = 1 or 2.
+			while (!str.matches("[0]")){
+				if (str.matches("1") ){
 					//singleProcess();
-					System.out.println("Choice: is " + myInput);
+					System.out.println("Choice: is " + str);
 				}
-				else if (myInput == "2" ){
+				else if (str.matches("2") ){
 					//batchProcess();
-					System.out.println("Choice: is " + myInput);
-				}
-				else if (myInput == "0") {
-					System.out.println("Choice: is " + myInput);
-					break;
+					System.out.println("Choice: is " + str);
 				}
 				else{
-					System.out.println("!!!!\n");
-					System.out.println("!!!! Error... Irregular input detected. Please try again...\n");
-					System.out.println("!!!!\n");
-					//break;
+					System.out.println("!!!!");
+					System.out.println("!!!! Error... Irregular input " + "[" + str + "]" +" detected. Please try again...");
+					System.out.println("!!!!");
 				}
-				System.out.println("***************************************************************************************\n");
-				System.out.println("***                                                                                 ***\n");
-				System.out.println("***                       PLEASE ENTER YOUR DESIRED OPTION                          ***\n");	
-				System.out.println("***                       Enter  0 ==> End the Program,                             ***\n");
-				System.out.println("***                       Enter  1 ==> Single Item Purchase                         ***\n");            
-				System.out.println("***                       Enter  2 ==> Multiple Purchase from .CSV file             ***\n"); 
-				System.out.println("***                                                                                 ***\n");
-				System.out.println("***************************************************************************************\n");
-				System.out.println("***************************************************************************************\n");
-				System.out.println("Choice: ");
-				//scanf("%s", &resp);
-				myInput = keyboard.nextLine();
+				
+				System.out.println("***************************************************************************************");
+				System.out.println("***                                                                                 ***");
+				System.out.println("***                       PLEASE ENTER YOUR DESIRED OPTION                          ***");	
+				System.out.println("***                       Enter  0 ==> End the Program,                             ***");
+				System.out.println("***                       Enter  1 ==> Single Item Purchase                         ***");            
+				System.out.println("***                       Enter  2 ==> Multiple Purchase from .CSV file             ***"); 
+				System.out.println("***                                                                                 ***");
+				System.out.println("***************************************************************************************");
+				System.out.println("***************************************************************************************");
+				//System.out.println("Choice: ");
+				System.out.println("Enter your Choice: ");
+				str = sin.nextLine();
+				//System.out.println("You entered string " + str);
+				
 			}
+			System.out.println("You entered string " + str + " - END of Program...");
+			sin.close();
 			//return 0;
 		}
 
